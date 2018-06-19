16f6a72ae754c0918a498f04d9e45b7b3018808b
raviyasas <raviyasas@live.com>
2018-06-19 16:25:14 +0530

    Initial commit

---
+++ src/main/java/com/app/dockerdemo/DockerDemoApplication.java
@@ -0,0 +1,26 @@
+package com.app.dockerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemoApplication.class, args);
+    }
+}
+
+
+@RestController
+class HelloController {
+
+    @GetMapping("/api/hello")
+    public String sayHello(){
+        return "Hello Spring Boot";
+    }
+
+}
